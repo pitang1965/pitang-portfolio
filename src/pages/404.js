d9fda90ba7b8601ef927afba235bfaad1fdbdfcd
Shoji Makino <pitang1965@gmail.com>
2021-05-15 08:44:25 +0900

    update modules

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-export default () => {
+const NotFound = () => {
   return (
     <div>
       <h1>
@@ -18,3 +18,5 @@ export default () => {
     </div>
   )
 }
+
+export default NotFound;
